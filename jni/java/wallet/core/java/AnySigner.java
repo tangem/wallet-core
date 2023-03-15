17e0d169bb3175f19fdb377b7aa27c27b8039e7d
Doston Kamalov <dkamalov@tangem.com>
2023-03-15 17:44:06 +0500

    Rename property

--- jni/java/wallet/core/java/AnySigner.java
+++ jni/java/wallet/core/java/AnySigner.java
@@ -34,9 +34,9 @@ public class AnySigner {
     }
     public static native byte[] nativePlan(byte[] data, int coin);
 
-    public static <T extends MessageLite> T signExternally(MessageLite inputData, CoinType coin, Parser<T> parser, Signer signer) throws Exception {
-        byte[] data = inputData.toByteArray();
-        byte[] outputData = nativeSignExternally(signer.getPublicKey().data(), data, coin.value(), signer);
+    public static <T extends MessageLite> T signExternally(MessageLite input, CoinType coin, Parser<T> parser, Signer signer) throws Exception {
+        byte[] inputData = input.toByteArray();
+        byte[] outputData = nativeSignExternally(signer.getPublicKey().data(), inputData, coin.value(), signer);
         T output = parser.parseFrom(outputData);
         outputData = null;
         return output;
